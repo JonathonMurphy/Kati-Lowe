74fad43451e0a1c970778d9c195f056c21e37e58
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-10-15 11:01:04 -0400

    Added gtag

--- require/header.php
+++ require/header.php
@@ -8,6 +8,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117935552-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-117935552-3');
+    </script>
+
+
     <!-- CSS -->
     <?php
     if($Is_Index == true){
