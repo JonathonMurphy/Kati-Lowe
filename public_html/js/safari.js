29971b098374df0044f2579055f26363c9f10416
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-10-23 10:18:16 -0400

    safari backgroung-img patch #1

--- public_html/js/safari.js
+++
@@ -1,9 +0,0 @@
-$({
-  if (navigator.userAgent.search("Safari") {
-    $("parallax_img").css("background-size": "contain", "background-attachment": "scroll");
-  }
-  var viewport = $( window ).width();;
-  if (viewport < 800) {
-
-  }
-})
