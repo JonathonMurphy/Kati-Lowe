19c0ccace36e91557611208e07497717a1249aa3
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2019-03-21 08:05:51 -0400

    ip geolocation

--- public_html/node/contactMeSubmissionHandler.js
+++ public_html/node/contactMeSubmissionHandler.js
@@ -1,12 +1,10 @@
-// TODO: Add in geolocation metadata for POST requests
 'use strict';
 const http = require('http'),
       fs = require('fs'),
+      stream = require('stream'),
       nameRegex = /([a-zA-Z\s-])/g,
       emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g,
       messageRegex = /([a-zA-Z0-9\s.,?!$%&])/gm;
-
-let messageIndex = 0;
 
 http.createServer((request, response) => {
     let body = [];
@@ -21,9 +19,7 @@ http.createServer((request, response) => {
       body.content.message = body.content.message.match(messageRegex).join("");
       body.metadata.ipAddress = request.connection.remoteAddress.replace(/^.*:/, '');
       console.log(body);
-      // fs.writeFile('./contact'+messageIndex+'.json', JSON.stringify(body));
-      fs.writeFile('testLog.json', JSON.stringify(body));
-      messageIndex++;
+      fs.writeFile('tempContactMe.json', JSON.stringify(body));
     });
     response.end();
   }).listen(3000);
--- public_html/node/ipLookup.js
+++ public_html/node/ipLookup.js
@@ -1,31 +1,34 @@
 'use strict';
-// url + ipAddress
 const fs = require('fs'),
       http = require('https'),
       oauth = require('./oauth.js'),
-      log = require('./testLog.json'),
+      log = require('./tempContactMe.json'),
       apiKey = JSON.stringify(oauth.apiKey).replace(/"/g,""),
       ipAddress = JSON.stringify(log.metadata.ipAddress).replace(/"/g,"");
 
-let options = {
-  "method": "GET",
-  "host": 'api.ipgeolocation.io',
-  "path": '/ipgeo?apiKey='+apiKey+'&ip='+ipAddress,
-  "port": 443,
-  "headers": {
-    "Accept": "application/json"
-  }
-};
+let messageIndex = 0,
+    options = {
+      "method": "GET",
+      "host": 'api.ipgeolocation.io',
+      "path": '/ipgeo?apiKey='+apiKey+'&ip='+ipAddress,
+      "port": 443,
+      "headers": {
+        "Accept": "application/json"
+      }
+    };
 
-http.get(options, (response) => {
-  let data = '';
-  response.on('data', (chunk) => {
-    data += chunk;
+fs.watch('tempContactMe.json', () => {
+  http.get(options, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    response.on("end", () => {
+      log.metadata = JSON.parse(data);
+      fs.writeFile('testLog'+messageIndex+'.json', JSON.stringify(log));
+      messageIndex++;
+    });
+  }).on('error', (err) => {
+    console.log(err)
   });
-  response.on("end", () => {
-    log.metadata = JSON.parse(data);
-    fs.writeFile('testLog2.json', JSON.stringify(log));
-  });
-}).on('error', (err) => {
-  console.log(err)
-});
+})
